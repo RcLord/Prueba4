c8a60ad19d8ece2c725a7d2b03631bdd549cd1f2
rsalazar <rsalazar@sistemas2.triveca.local>
2019-09-30 11:00:37 -0500

    Commit desde eclipse

---
+++ Prueba4/src/main/java/com/ejemplos/Prueba4/Prueba4Application.java
@@ -0,0 +1,14 @@
+package com.ejemplos.Prueba4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prueba4Application.class, args);
+		System.out.println("Hola mundo, por 4ta vez");
+	}
+
+}
